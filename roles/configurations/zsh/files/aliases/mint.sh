a04f12c8fb9a5b376aaaaa6f3a4e8e7be2f53065
root <showenotto@proton.me>
2024-01-23 22:54:01 +0200

    1.0

--- roles/configurations/zsh/files/aliases/mint.sh
+++ roles/configurations/zsh/files/aliases/mint.sh
@@ -1,5 +1,6 @@
 alias mint-backup="sudo ansible-playbook /home/showen/mint-build/backup.yml"
-alias ssd-backup="~/showenotto/scripts/ssd-backup.sh"
+alias showen-backup="~/showen/showen-backup.sh"
+alias ssd-backup="~/showen/scripts/ssd-backup.sh"
 alias log-out="cinnamon-session-quit --logout --force"
 alias start-kali="VBoxManage startvm \"Kali Linux\""
 alias stop-kali="VBoxManage controlvm \"Kali Linux\" poweroff"
